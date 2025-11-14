3066f4844bbfae1c1f539775d108b0b270f8a04e
Ignacio Jonas <ignaciojonas@gmail.com>
2025-11-13 21:23:33 -0300

    feat(config): add DB_SSL configuration for database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,6 +14,7 @@ import { CoffeesModule } from './coffees/coffees.module';
       validationSchema: Joi.object({
         DATABASE_URL: Joi.string().uri().optional(),
         DB_SYNC: Joi.boolean().truthy('true').falsy('false').default(true),
+        DB_SSL: Joi.boolean().truthy('true').falsy('false').default(false),
       }),
     }),
     CoffeesModule,
@@ -24,6 +25,7 @@ import { CoffeesModule } from './coffees/coffees.module';
         url: config.get<string>('DATABASE_URL') ?? undefined,
         autoLoadEntities: true,
         synchronize: config.get<boolean>('DB_SYNC'),
+        ssl: config.get<boolean>('DB_SSL'),
       }),
     }),
   ],
